5cf00f43780ca75d45ffbe947cf7227b8b062078
rajilateef <rajilateef.rl@gmail.com>
2021-10-01 16:48:58 +0100

    react initiation

--- src/components/breadcumb.jsx
+++ src/components/breadcumb.jsx
@@ -1,8 +1,24 @@
 import React, { Component } from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 class Breadcumb extends React.Component {
-    
+
     render() { 
-        return <div></div>;
+        return (
+
+        <Navbar bg="light" variant="light" >
+            <Container>
+           
+            <Nav className="me-auto" className="bread">
+            <Nav.Link href="">All</Nav.Link>
+            <Nav.Link href="">Director</Nav.Link>
+            <Nav.Link href="">Co-Founder</Nav.Link>
+            <Nav.Link href="">CEO</Nav.Link>
+            <Nav.Link href="">CIO</Nav.Link>
+            <Nav.Link href="">CFO</Nav.Link>
+            </Nav>
+            </Container>
+        </Navbar>
+        )
     }
 }
  
--- src/components/profileListing.jsx
+++ src/components/profileListing.jsx
@@ -5,6 +5,7 @@ import '../App.css';
 import menu from '../icons/menu.png';
 import filter from '../icons/􀘵 Filter.svg';
 import search from '../icons/􀒒 Search.svg';
+import Breadcumb from './breadcumb';
 class Listing extends React.Component {
     render() { 
         return(
@@ -15,31 +16,7 @@ class Listing extends React.Component {
                   <Col xs={12} md={6}>
                                {/* navbar content for Listing page */}
 
-                {/* <Nav className="container">
-                  
-                    <Nav.Item className="item">
-                      <Nav.Link to="/home"> <img src={menu} /></Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item className="item">
-                      <Link to="">Member Directory</Link>
-                    </Nav.Item>
-                    <Nav.Item className="item">
-                      <Nav.Link>img</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link>img</Nav.Link>
-                    </Nav.Item>
-                  
-                  </Nav> */}
-
-  <Navbar className="Wrap">
-  {/* <Navbar.Brand href=""><img src={menu}/></Navbar.Brand>
-  <Navbar.Toggle aria-controls="basic-navbar-nav" /> */}
-    {/* <Nav className="mr-auto">
-      <Nav.Link href="#home">Member Directory</Nav.Link>
-      <Nav.Link href="#link">Link</Nav.Link>
-     
-                      </Nav> */}
+                    <Navbar className="Wrap">
 
                       <Nav style={{ margin: '55px 0px 13px 20px' }}>
 
@@ -52,12 +29,14 @@ class Listing extends React.Component {
                        
                         <Nav.Item className="" style={{ marginLeft: '20px !important' }} >
                           <Nav.Link to="" ><img src={filter} /></Nav.Link>
+
                         </Nav.Item>
                         <Nav.Item className="" style={{ paddingLeft: '10px' }}>
                           <Nav.Link ><img src={search} /></Nav.Link>
                         </Nav.Item>
                       </Nav>
-                      </Navbar>
+                    </Navbar>
+                    <Breadcumb/>
            
                   </Col>
                 </Row>
