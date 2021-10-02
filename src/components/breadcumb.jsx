c9e61d7275bff535ffa65cb3dc6e60918e1dfea2
rajilateef <rajilateef.rl@gmail.com>
2021-10-02 22:42:30 +0100

    react initiation

--- src/components/breadcumb.jsx
+++ src/components/breadcumb.jsx
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import { Navbar, Nav, Container } from 'react-bootstrap';
-class Breadcumb extends React.Component {
-
-    render() { 
-        return (
-
-        <Navbar bg="light" variant="light" >
+import { Navbar, Nav, Container, ListGroup, Card, Row, Col } from 'react-bootstrap';
+import '../App.css';
+import image from '../icons/image.png';
+const Breadcumb = ({data}) => {
+    return (
+        <>
+        <Navbar bg="light" variant="light" style={{ width:'375' }} >
             <Container>
            
             <Nav className="me-auto" className="bread">
@@ -18,8 +18,42 @@ class Breadcumb extends React.Component {
             </Nav>
             </Container>
         </Navbar>
-        )
-    }
+
+            <div style={{ height: '30px', width: '100vw' }}>
+
+            </div>
+
+            {data.map((value, key) =>{ return (
+                <div key={key} style={{ backgroundColor: "white", width:'100%'}}>
+
+                   <Container style={{ padding: '0px', marginRight: '20px', marginLeft: '20px', width: '100vw'}}>
+            <Row style={{  backgroundColor: 'white', boxShadow: '0px 0px 1px black ', width: '100vw', padding: '0px 20px', left:'0'}} >
+                
+                <Col xs={4}>
+                    
+                    
+                    <Card.Body>
+                    <Card.Img src={image} />
+                    </Card.Body>
+                    
+                </Col>
+                <Col xs={8}>
+                <Card.Body style={{ width: '100%' }}>
+                        <Card.Title>{value.name}</Card.Title>
+                        <Card.Text style={{ fontSize: '12px' }}>
+                        {value.description}
+                        </Card.Text>
+                    </Card.Body>
+                </Col>
+
+            </Row>
+                   </Container>
+                </div>
+            )})}
+            
+     
+        </>
+    );
 }
- 
+
 export default Breadcumb;
--- src/components/profileListing.jsx
+++ src/components/profileListing.jsx
@@ -6,19 +6,66 @@ import menu from '../icons/menu.png';
 import filter from '../icons/􀘵 Filter.svg';
 import search from '../icons/􀒒 Search.svg';
 import Breadcumb from './breadcumb';
-class Listing extends React.Component {
-    render() { 
-        return(
-            <>
+import Data from '../Data.json';
+// class Listing extends React.Component {
+//     render() { 
+//         return(
+//             <>
 
-              <Container style={{ margin: "0px !important", padding: '0px' }}>
+//               <Container style={{ margin: "0px !important", padding: '0px' }}>
+//                 <Row>
+//                   <Col xs={12} md={6}>
+//                                {/* navbar content for Listing page */}
+
+//                     <Navbar className="Wrap" sticky="top">
+
+//                       <Nav style={{ margin: '45px 0px 13px 20px' }}>
+
+//                       <Nav.Item className="size">
+//                           <Nav.Link to="/home"> <img src={menu} /></Nav.Link>
+//                         </Nav.Item>
+//                         <Nav.Item style={{ paddingRight: '20px', paddingLeft: '30px' }}>
+//                           <Nav.Link to="" style={{ color: '#444444' }}>Members Directory</Nav.Link>
+//                         </Nav.Item>
+                       
+//                         <Nav.Item className="size" style={{ marginLeft: '20px !important' }} >
+//                           <Nav.Link to="" ><img src={filter} /></Nav.Link>
+
+//                         </Nav.Item>
+//                         <Nav.Item className="size" style={{ paddingLeft: '10px' }}>
+//                           <Nav.Link style={{ width: '20px' }}><img src={search} /></Nav.Link>
+//                         </Nav.Item>
+//                       </Nav>
+//                     </Navbar>
+//                     <Breadcumb data={Data}/>
+
+
+
+//                   </Col>
+//                 </Row>
+//               </Container>
+                                
+//             </>
+           
+//         ) 
+//     }
+// }
+ 
+// export default Listing;
+
+
+
+const profileListing = () => {
+  return (
+  
+       <Container style={{ margin: "0px !important", padding: '0px' }}>
                 <Row>
                   <Col xs={12} md={6}>
                                {/* navbar content for Listing page */}
 
-                    <Navbar className="Wrap">
+                    <Navbar className="Wrap"  fixed='top' style={{ position:"sticky", top: '0'}}>
 
-                      <Nav style={{ margin: '55px 0px 13px 20px' }}>
+                      <Nav style={{ margin: '45px 0px 13px 20px' }}>
 
                       <Nav.Item className="">
                           <Nav.Link to="/home"> <img src={menu} /></Nav.Link>
@@ -32,20 +79,19 @@ class Listing extends React.Component {
 
                         </Nav.Item>
                         <Nav.Item className="" style={{ paddingLeft: '10px' }}>
-                          <Nav.Link ><img src={search} /></Nav.Link>
+                          <Nav.Link style={{ width: '20px' }}><img src={search} /></Nav.Link>
                         </Nav.Item>
                       </Nav>
                     </Navbar>
-                    <Breadcumb/>
-           
+                    <Breadcumb data={Data}/>
+
+
+
                   </Col>
                 </Row>
               </Container>
-                                
-            </>
-           
-        ) 
-    }
+  
+  );
 }
- 
-export default Listing;
+
+export default profileListing;
